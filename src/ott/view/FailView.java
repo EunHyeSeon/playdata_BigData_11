79172555720f121bb8885f7c340e807a930a3742
EunHyeSeon <100753335+EunHyeSeon@users.noreply.github.com>
2022-10-05 20:45:00 +0900

    Delete src directory

--- src/ott/view/FailView.java
+++
@@ -1,9 +0,0 @@
-package ott.view;
-
-public class FailView {
-	public static void printMsg(String msg) {
-		System.out.println("⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯");
-		System.out.println(msg);
-	}
-
-}
